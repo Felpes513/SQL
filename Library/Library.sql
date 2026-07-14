08c89a12ea47e24eb2513fec67cae2c624b015ba
Felipe Souza Moreira <felipemoreira2003@gmail.com>
2026-07-14 18:40:28 -0300

    feat: Tabelas books_tb e categories_tb adicionadas

--- Library/Library.sql
+++ Library/Library.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE Library;
 
-CREATE TABLE users(
+CREATE TABLE users_tb(
 	user_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 	name VARCHAR(50),
 	cpf VARCHAR(11),
@@ -10,3 +10,19 @@ CREATE TABLE users(
 	email VARCHAR(45)
 );
 
+ALTER TABLE users RENAME TO users_tb;
+
+CREATE TABLE categories_tb(
+	category_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	hallway INT,
+	name VARCHAR(25)
+);
+
+CREATE TABLE books_tb(
+	book_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	title VARCHAR(45),
+	release DATE,
+	category_id BIGINT,
+
+	FOREIGN KEY (category_id) REFERENCES categories_tb(category_id)
+);
